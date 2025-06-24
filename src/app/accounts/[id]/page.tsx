af72444dc08ec760eda7c483104edf7a65c84eec
bflorestal <yayou7710@hotmail.fr>
2025-06-25 00:16:05 +0200

    fix: conditionally render SteamIcon based on account status

--- src/app/accounts/[id]/page.tsx
+++ src/app/accounts/[id]/page.tsx
@@ -22,7 +22,7 @@ export default async function AccountPage({
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <div className="flex items-center space-x-1">
-          <SteamIcon className="w-4 h-4 fill-white" />
+          {account.isSteam && <SteamIcon className="w-4 h-4 fill-white" />}
           <h1 className="text-2xl font-bold">{account.username}</h1>
         </div>
         <h2 className="text-lg font-semibold">
