aa39d5050992c6eb96b5c65857542e70dafb284e
bflorestal <yayou7710@hotmail.fr>
2025-07-20 18:17:26 +0200

    chore: add title to sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -5,6 +5,7 @@ export default function Sidebar() {
   return (
     <aside className="no-scrollbar font-semibold text-slate-200 w-full lg:w-64 h-full flex-col hidden lg:flex p-5">
       <div>
+        <h1 className="font-bold mb-4">Elsword Account Organizer</h1>
         <Link
           href="/"
           className="text-white flex space-x-2 hover:bg-gray-700/20 rounded-lg p-2"
