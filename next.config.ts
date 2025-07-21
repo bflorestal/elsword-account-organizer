14ac64afc4a0fe191e4a815f2017ce9e1a588886
bflorestal <yayou7710@hotmail.fr>
2025-07-22 00:15:43 +0200

    feat: add Dockerfile and .dockerignore

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [new URL("https://elwiki.net/wiki/images/**")],
   },
+  output: "standalone",
   serverExternalPackages: ["bun:sqlite"],
 };
 
